aa86212053ed8ad4ee47c8923d6f6eba72269984
n-aa-g <wlsdnjs0124@gmail.com>
2023-06-05 19:41:53 +0900

    Feat: Pawn Class 수정
    색에 관련된 상수를 만들었습니다.
    하얀색을 기본으로 하는 기본 생성자를 만들었습니다.

--- src/main/java/Pawn.java
+++ src/main/java/Pawn.java
@@ -1,6 +1,12 @@
 public class Pawn {
 
+    public static final String COLOR_BLACK = "black";
+    public static final String COLOR_WHITE = "white";
     private final String color;
+
+    public Pawn() {
+        this.color = COLOR_WHITE;
+    }
 
     public Pawn(String color) {
         this.color = color;
