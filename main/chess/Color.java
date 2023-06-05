211b631bce2ec709ca91486294da911cc65209a7
yeonise <cerealnum@gmail.com>
2023-06-05 12:05:07 +0900

    refactor : package 정리
    - chess package 하위에 piece package가 존재합니다.
    - chess package : Board, Color
    - piece package : Pawn
    - test 폴더도 함께 정리합니다.

--- main/chess/Color.java
+++ main/chess/Color.java
@@ -1,4 +1,4 @@
-package piece;
+package chess;
 
 public enum Color {
     WHITE, BLACK
--- main/chess/piece/Pawn.java
+++ main/chess/piece/Pawn.java
@@ -1,8 +1,16 @@
-package piece;
+package chess.piece;
+
+import chess.Color;
+
+import static chess.Color.WHITE;
 
 public class Pawn {
 
     private final Color color;
+
+    public Pawn() {
+        this.color = WHITE;
+    }
 
     public Pawn(final Color color) {
         this.color = color;
