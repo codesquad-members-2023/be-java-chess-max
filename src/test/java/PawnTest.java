658c838d852b7efc07e0cc5c35791efbf41671c0
n-aa-g <wlsdnjs0124@gmail.com>
2023-06-05 18:18:29 +0900

    Test: createPawns 테스트 리팩토링
    "white", "black"이 중복되어 각각 local variable로 지정하고 코드를 수정했습니다.

--- src/test/java/PawnTest.java
+++ src/test/java/PawnTest.java
@@ -8,10 +8,12 @@ public class PawnTest {
     @Test
     @DisplayName("흰색, 검은색 폰이 생성된다")
     void createPawns() {
-        Pawn onePawn = new Pawn("white");
-        Pawn otherPawn = new Pawn("black");
+        String white = "white";
+        String black = "black";
+        Pawn onePawn = new Pawn(white);
+        Pawn otherPawn = new Pawn(black);
 
-        assertThat(onePawn.getColor()).isEqualTo("white");
-        assertThat(otherPawn.getColor()).isEqualTo("black");
+        assertThat(onePawn.getColor()).isEqualTo(white);
+        assertThat(otherPawn.getColor()).isEqualTo(black);
     }
 }
