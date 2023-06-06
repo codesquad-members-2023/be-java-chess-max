2e3734fb0572d0a4d72f87e380d30accc12acb0c
n-aa-g <wlsdnjs0124@gmail.com>
2023-06-06 15:52:34 +0900

    Feat: Application 생성
    Scanner를 이용하여 값을 입력받고 게임을 실행하는 Application 클래스를 생성했습니다.

---
+++ src/main/java/Application.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ChessGame game = new ChessGame();
+
+        do {
+            String input = scanner.nextLine().trim();
+            game.play(input);
+            System.out.println(game.printResult());
+        } while(game.doesGameContinue());
+    }
+}
