ee0b0e88cd36cc22cad53ee63b94e7be59929dac
n-aa-g <wlsdnjs0124@gmail.com>
2023-06-07 15:04:58 +0900

    Feat: Type enum 추가
    드디어... 이넘을 씁니다. 상수가 쌓일 때마다 괴로웠는데 드디어 간략하게 관리를 하게 됩니다.
    Type enum에 shape 필드를 넣어 모양 정보를 넣었습니다.
    또한 createBlank() 메서드를 생성하여 빈 칸을 만들 수 있게 했습니다.

--- src/main/java/pieces/Piece.java
+++ src/main/java/pieces/Piece.java
@@ -2,104 +2,104 @@ package pieces;
 
 public class Piece {
 
-    public enum Color{
-        WHITE, BLACK, NOCOLOR
+    public enum Color {
+
+        WHITE, BLACK, NOCOLOR;
     }
 
-    private static final String PAWN_TYPE = "pawn";
-    private static final String ROOK_TYPE = "rook";
-    private static final String KNIGHT_TYPE = "knight";
-    private static final String BISHOP_TYPE = "bishop";
-    private static final String QUEEN_TYPE = "queen";
-    private static final String KING_TYPE = "king";
-    private static final String BLACK_PAWN_FIGURE = "P";
-    private static final String BLACK_ROOK_FIGURE = "R";
-    private static final String BLACK_KNIGHT_FIGURE = "N";
-    private static final String BLACK_BISHOP_FIGURE = "B";
-    private static final String BLACK_QUEEN_FIGURE = "Q";
-    private static final String BLACK_KING_FIGURE = "K";
-    private static final String WHITE_PAWN_FIGURE = "p";
-    private static final String WHITE_ROOK_FIGURE = "r";
-    private static final String WHITE_KNIGHT_FIGURE = "n";
-    private static final String WHITE_BISHOP_FIGURE = "b";
-    private static final String WHITE_QUEEN_FIGURE = "q";
-    private static final String WHITE_KING_FIGURE = "k";
+    public enum Type {
+        
+        PAWN('p'), ROOK('r'), KNIGHT('n'), BISHOP('b'), 
+        QUEEN('q'), KING('k'), NOTYPE('.');
 
+        private final char shape;
+
+        private Type(char shape) {
+            this.shape = shape;
+        }
+
+        public char getShape() {
+            return shape;
+        }
+    }
+    
     private final Color color;
-    private final String type;
-    private final String figure;
+    private final Type type;
 
-    private Piece(Color color, String type, String figure) {
+    private Piece(Color color, Type type) {
         this.color = color;
         this.type = type;
-        this.figure = figure;
     }
 
     public static Piece createBlackPawn() {
-        return new Piece(Color.BLACK, PAWN_TYPE, BLACK_PAWN_FIGURE);
+        return new Piece(Color.BLACK, Type.PAWN);
     }
 
     public static Piece createBlackRook() {
-        return new Piece(Color.BLACK, ROOK_TYPE, BLACK_ROOK_FIGURE);
+        return new Piece(Color.BLACK, Type.ROOK);
     }
 
     public static Piece createBlackKnight() {
-        return new Piece(Color.BLACK, KNIGHT_TYPE, BLACK_KNIGHT_FIGURE);
+        return new Piece(Color.BLACK, Type.KNIGHT);
     }
 
     public static Piece createBlackBishop() {
-        return new Piece(Color.BLACK, BISHOP_TYPE, BLACK_BISHOP_FIGURE);
+        return new Piece(Color.BLACK, Type.BISHOP);
     }
 
     public static Piece createBlackQueen() {
-        return new Piece(Color.BLACK, QUEEN_TYPE, BLACK_QUEEN_FIGURE);
+        return new Piece(Color.BLACK, Type.QUEEN);
     }
 
     public static Piece createBlackKing() {
-        return new Piece(Color.BLACK, KING_TYPE, BLACK_KING_FIGURE);
+        return new Piece(Color.BLACK, Type.KING);
     }
 
     public static Piece createWhitePawn() {
-        return new Piece(Color.WHITE, PAWN_TYPE, WHITE_PAWN_FIGURE);
+        return new Piece(Color.WHITE, Type.PAWN);
     }
 
     public static Piece createWhiteRook() {
-        return new Piece(Color.WHITE, ROOK_TYPE, WHITE_ROOK_FIGURE);
+        return new Piece(Color.WHITE, Type.ROOK);
     }
 
     public static Piece createWhiteKnight() {
-        return new Piece(Color.WHITE, KNIGHT_TYPE, WHITE_KNIGHT_FIGURE);
+        return new Piece(Color.WHITE, Type.KNIGHT);
     }
 
     public static Piece createWhiteBishop() {
-        return new Piece(Color.WHITE, BISHOP_TYPE, WHITE_BISHOP_FIGURE);
+        return new Piece(Color.WHITE, Type.BISHOP);
     }
 
     public static Piece createWhiteQueen() {
-        return new Piece(Color.WHITE, QUEEN_TYPE, WHITE_QUEEN_FIGURE);
+        return new Piece(Color.WHITE, Type.QUEEN);
     }
 
     public static Piece createWhiteKing() {
-        return new Piece(Color.WHITE, KING_TYPE, WHITE_KING_FIGURE);
+        return new Piece(Color.WHITE, Type.KING);
+    }
+
+    public static Piece createBlank() {
+        return new Piece(Color.NOCOLOR, Type.NOTYPE);
     }
 
     public boolean isBlackPawn() {
-        return color.equals(Color.BLACK) && type.equals(PAWN_TYPE);
+        return color.equals(Color.BLACK) && type.equals(Type.PAWN);
     }
 
     public boolean isWhitePawn() {
-        return color.equals(Color.WHITE) && type.equals(PAWN_TYPE);
+        return color.equals(Color.WHITE) && type.equals(Type.PAWN);
     }
 
     public Color getColor() {
         return color;
     }
 
-    public String getType() {
+    public Type getType() {
         return type;
     }
 
-    public String getFigure() {
-        return figure;
+    public char getShape() {
+        return type.getShape();
     }
 }
