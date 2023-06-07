88e4120049981caf1b5e7f0f38c76b68ff8db556
n-aa-g <wlsdnjs0124@gmail.com>
2023-06-07 14:52:33 +0900

    Feat(Piece): Color enum 추가

--- src/main/java/pieces/Piece.java
+++ src/main/java/pieces/Piece.java
@@ -2,8 +2,10 @@ package pieces;
 
 public class Piece {
 
-    private static final String BLACK_COLOR = "black";
-    private static final String WHITE_COLOR = "white";
+    public enum Color{
+        WHITE, BLACK, NOCOLOR
+    }
+
     private static final String PAWN_TYPE = "pawn";
     private static final String ROOK_TYPE = "rook";
     private static final String KNIGHT_TYPE = "knight";
@@ -23,73 +25,73 @@ public class Piece {
     private static final String WHITE_QUEEN_FIGURE = "q";
     private static final String WHITE_KING_FIGURE = "k";
 
-    private final String color;
+    private final Color color;
     private final String type;
     private final String figure;
 
-    private Piece(String color, String type, String figure) {
+    private Piece(Color color, String type, String figure) {
         this.color = color;
         this.type = type;
         this.figure = figure;
     }
 
     public static Piece createBlackPawn() {
-        return new Piece(BLACK_COLOR, PAWN_TYPE, BLACK_PAWN_FIGURE);
+        return new Piece(Color.BLACK, PAWN_TYPE, BLACK_PAWN_FIGURE);
     }
 
     public static Piece createBlackRook() {
-        return new Piece(BLACK_COLOR, ROOK_TYPE, BLACK_ROOK_FIGURE);
+        return new Piece(Color.BLACK, ROOK_TYPE, BLACK_ROOK_FIGURE);
     }
 
     public static Piece createBlackKnight() {
-        return new Piece(BLACK_COLOR, KNIGHT_TYPE, BLACK_KNIGHT_FIGURE);
+        return new Piece(Color.BLACK, KNIGHT_TYPE, BLACK_KNIGHT_FIGURE);
     }
 
     public static Piece createBlackBishop() {
-        return new Piece(BLACK_COLOR, BISHOP_TYPE, BLACK_BISHOP_FIGURE);
+        return new Piece(Color.BLACK, BISHOP_TYPE, BLACK_BISHOP_FIGURE);
     }
 
     public static Piece createBlackQueen() {
-        return new Piece(BLACK_COLOR, QUEEN_TYPE, BLACK_QUEEN_FIGURE);
+        return new Piece(Color.BLACK, QUEEN_TYPE, BLACK_QUEEN_FIGURE);
     }
 
     public static Piece createBlackKing() {
-        return new Piece(BLACK_COLOR, KING_TYPE, BLACK_KING_FIGURE);
+        return new Piece(Color.BLACK, KING_TYPE, BLACK_KING_FIGURE);
     }
 
     public static Piece createWhitePawn() {
-        return new Piece(WHITE_COLOR, PAWN_TYPE, WHITE_PAWN_FIGURE);
+        return new Piece(Color.WHITE, PAWN_TYPE, WHITE_PAWN_FIGURE);
     }
 
     public static Piece createWhiteRook() {
-        return new Piece(WHITE_COLOR, ROOK_TYPE, WHITE_ROOK_FIGURE);
+        return new Piece(Color.WHITE, ROOK_TYPE, WHITE_ROOK_FIGURE);
     }
 
     public static Piece createWhiteKnight() {
-        return new Piece(WHITE_COLOR, KNIGHT_TYPE, WHITE_KNIGHT_FIGURE);
+        return new Piece(Color.WHITE, KNIGHT_TYPE, WHITE_KNIGHT_FIGURE);
     }
 
     public static Piece createWhiteBishop() {
-        return new Piece(WHITE_COLOR, BISHOP_TYPE, WHITE_BISHOP_FIGURE);
+        return new Piece(Color.WHITE, BISHOP_TYPE, WHITE_BISHOP_FIGURE);
     }
 
     public static Piece createWhiteQueen() {
-        return new Piece(WHITE_COLOR, QUEEN_TYPE, WHITE_QUEEN_FIGURE);
+        return new Piece(Color.WHITE, QUEEN_TYPE, WHITE_QUEEN_FIGURE);
     }
 
     public static Piece createWhiteKing() {
-        return new Piece(WHITE_COLOR, KING_TYPE, WHITE_KING_FIGURE);
+        return new Piece(Color.WHITE, KING_TYPE, WHITE_KING_FIGURE);
     }
 
     public boolean isBlackPawn() {
-        return color.equals(BLACK_COLOR) && type.equals(PAWN_TYPE);
+        return color.equals(Color.BLACK) && type.equals(PAWN_TYPE);
     }
 
     public boolean isWhitePawn() {
-        return color.equals(WHITE_COLOR) && type.equals(PAWN_TYPE);
+        return color.equals(Color.WHITE) && type.equals(PAWN_TYPE);
     }
 
-    public String getColor() {
+    public Color getColor() {
         return color;
     }
 
