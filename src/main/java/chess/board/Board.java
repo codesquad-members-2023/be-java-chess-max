62854939acaf1e93762f25a937214d981af426ad
Jeongyong Park <1239824@naver.com>
2023-06-07 12:04:49 +0900

    feat: 2차원 배열로 체스말을 관리하도록 변경
    - 후에 체스말을 움직일 때 `List`보다는 배열을 통해 관리하는 것이 용이할 것이라 생각되어 변경한다.

--- src/main/java/chess/board/Board.java
+++ src/main/java/chess/board/Board.java
@@ -1,27 +1,42 @@
 package chess.board;
 
+import chess.color.Color;
+import chess.pieces.Dummy;
 import chess.pieces.Pawn;
+import chess.pieces.Piece;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Board {
 
-	private final List<Pawn> pawns;
+	private static final int BOARD_SIZE = 8;
+
+	private final Piece[][] board;
 
 	public Board() {
-		this.pawns = new ArrayList<>();
+		this.board = new Piece[BOARD_SIZE][BOARD_SIZE];
 	}
 
-	public int size() {
-		return pawns.size();
+	public void initialize() {
+		for (int i = 0; i < BOARD_SIZE; i++) {
+			Arrays.fill(board[i], new Dummy());
+		}
+
+		int initialPawnRow = 1;
+		Arrays.fill(board[initialPawnRow], new Pawn(Color.BLACK));
+		Arrays.fill(board[BOARD_SIZE - initialPawnRow - 1], new Pawn(Color.WHITE));
 	}
 
-	public void addPiece(final Pawn piece) {
-		pawns.add(piece);
-	}
+	public String print() {
+		StringBuilder boardFigure = new StringBuilder();
+		for (int row = 0; row < BOARD_SIZE; row++) {
+			String boardLine = Arrays.stream(board[row])
+					.map(Object::toString)
+					.collect(Collectors.joining());
+			boardFigure.append(boardLine).append("\n");
+		}
 
-	public Pawn findPawn(final int idx) {
-		return pawns.get(idx);
+		return boardFigure.toString();
 	}
 }
---
+++ src/main/java/chess/pieces/Dummy.java
@@ -0,0 +1,9 @@
+package chess.pieces;
+
+public class Dummy extends Piece {
+
+	@Override
+	public String toString() {
+		return ".";
+	}
+}
