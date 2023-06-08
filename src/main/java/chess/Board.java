44cdea234488ba5853a4ad794fb9d4a9aa6c9f57
JIHYE <jdh324@naver.com>
2023-06-08 16:11:16 +0900

    feat: 체스 판 초기화

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -1,30 +1,57 @@
 package chess;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.stream.Collectors;
 
 import chess.pieces.Pawn;
 
 public class Board {
-	private final List<Pawn> pawns;
+	public final static int PAWN_NUMBER = 8;
+
+	private final ArrayList<Pawn> whitePawns;
+	private final ArrayList<Pawn> blackPawns;
 
 	public Board() {
-		this.pawns = new ArrayList<>();
+		this.whitePawns = new ArrayList<>();
+		this.blackPawns = new ArrayList<>();
 	}
 
-	public void add(Pawn pawn) {
-		pawns.add(pawn);
+	public void addWhitePawn(Pawn pawn) {
+		whitePawns.add(pawn);
+	}
+
+	public void addBlackPawn(Pawn pawn) {
+		blackPawns.add(pawn);
 	}
 
 	public int size() {
-		return pawns.size();
+		return whitePawns.size() + blackPawns.size();
 	}
 
-	public String findPawnColor(int index) {
-		return pawns.get(index).getColor();
+	public void initialize() {
+		for (int i = 0; i < PAWN_NUMBER; i++) {
+			Pawn whitePawn = new Pawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
+			addWhitePawn(whitePawn);
+		}
+		for (int i = 0; i < PAWN_NUMBER; i++) {
+			Pawn blackPawn = new Pawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
+			addBlackPawn(blackPawn);
+		}
 	}
-
-	public void initialize() {}
 
 	public void print() {}
+
+	public String getWhitePawnsResult() {
+		return getPawnsResult(whitePawns);
+	}
+
+	public String getBlackPawnsResult() {
+		return getPawnsResult(blackPawns);
+	}
+
+	public String getPawnsResult(ArrayList<Pawn> pawns) {
+		return pawns.stream()
+			.map(p -> Character.toString(p.getRepresentation()))
+			.collect(Collectors.joining());
+	}
 }
