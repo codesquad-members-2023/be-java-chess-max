89a989faf1d75d3683df1a8ea38a0a071ceced67
Jeongyong Park <1239824@naver.com>
2023-06-09 21:12:56 +0900

    feat: equals & hashcode 오버라이딩
    - `Position`은 x좌표와 y좌표가 동일하면 같은 것이기 때문에 각각을 오버라이딩

--- src/main/java/chess/board/Position.java
+++ src/main/java/chess/board/Position.java
@@ -3,6 +3,8 @@ package chess.board;
 import chess.exception.BusinessException;
 import chess.exception.ErrorCode;
 
+import java.util.Objects;
+
 public class Position {
 
 	private static final int BOARD_SIZE = 8;
@@ -34,5 +36,18 @@ public class Position {
 
 	public int getY() {
 		return y;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Position position = (Position) o;
+		return x == position.x && y == position.y;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(x, y);
 	}
 }
