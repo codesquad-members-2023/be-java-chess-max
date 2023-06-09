9f44fc99c4da8503a78242007c982cf0e45773bf
Jeongyong Park <1239824@naver.com>
2023-06-09 19:38:23 +0900

    test: 체스보드 위에 놓여져 있는 기물의 개수를 세는 기능 테스트코드 작성

--- src/test/java/chess/board/BoardTest.java
+++ src/test/java/chess/board/BoardTest.java
@@ -1,8 +1,15 @@
 package chess.board;
 
+import chess.color.Color;
+import chess.pieces.type.Type;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -41,5 +48,36 @@ class BoardTest {
 				.append("pppppppp").append(newLine)
 				.append("rnbqkbnr");
 		return figure.toString();
+	}
+
+	@DisplayName("기물의 개수를 셀 때 기물의 타입과 색깔이 주어지면 해당 기물의 개수가 반환된다.")
+	@MethodSource("provideTypeAndColor")
+	@ParameterizedTest
+	void givenPieceTypeAndColor_whenCountPieces_thenReturnsCountOfPieces(Type type, Color color, int expectedCount) {
+		// given
+		sut.initialize();
+
+		// when
+		int count = sut.countPieces(type, color);
+
+		// then
+		assertThat(count).isEqualTo(expectedCount);
+	}
+
+	private static Stream<Arguments> provideTypeAndColor() {
+		return Stream.of(
+				Arguments.of(Type.PAWN, Color.BLACK, 8),
+				Arguments.of(Type.PAWN, Color.WHITE, 8),
+				Arguments.of(Type.ROOK, Color.BLACK, 2),
+				Arguments.of(Type.ROOK, Color.WHITE, 2),
+				Arguments.of(Type.KNIGHT, Color.BLACK, 2),
+				Arguments.of(Type.KNIGHT, Color.WHITE, 2),
+				Arguments.of(Type.BISHOP, Color.BLACK, 2),
+				Arguments.of(Type.BISHOP, Color.WHITE, 2),
+				Arguments.of(Type.QUEEN, Color.BLACK, 1),
+				Arguments.of(Type.QUEEN, Color.WHITE, 1),
+				Arguments.of(Type.KING, Color.BLACK, 1),
+				Arguments.of(Type.KING, Color.WHITE, 1)
+		);
 	}
 }
