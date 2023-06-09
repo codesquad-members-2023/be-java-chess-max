052d3198eeed1862fb859a2848958785f58f0347
yeonise <cerealnum@gmail.com>
2023-06-09 12:01:11 +0900

    feat : [ChessGame.java] move 명령어를 처리하는 기능 구현
    - main에 move 명령을 처리하는 기능을 추가합니다.
    - 이동을 완료한 후 체스판을 출력합니다.

--- main/chess/ChessGame.java
+++ main/chess/ChessGame.java
@@ -6,6 +6,7 @@ public class ChessGame {
 
     public static final String RUN_COMMAND = "start";
     public static final String EXIT_COMMAND = "end";
+    public static final String MOVE_COMMAND = "move";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -13,12 +14,19 @@ public class ChessGame {
         System.out.println("--- Chess Game ---");
         System.out.println("--- 게임 시작은 start, 게임 종료는 end를 입력하세요 ---");
 
+        Board board = new Board();
+
         while (true) {
             String command = scanner.nextLine();
 
             if (command.equals(RUN_COMMAND)) {
-                Board board = new Board();
                 board.initialize();
+                System.out.println(board.show());
+            } else if (command.startsWith(MOVE_COMMAND)) {
+                final String[] positions = command.split(" ");
+                final String source = positions[1];
+                final String target = positions[2];
+                board.move(source, target);
                 System.out.println(board.show());
             } else if (command.equals(EXIT_COMMAND)) {
                 break;
