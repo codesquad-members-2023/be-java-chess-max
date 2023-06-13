dde8218c0760127a34da6af37af02bd9b9295d04
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-06-13 22:25:26 +0900

    test: 체스 게임에서 선공이 백색인 상태에서 흑백 기물 이동시 예외가 발생하는 테스트 추가

---
+++ src/test/java/kr/codesqaud/chessgame/chess/ChessGameTest.java
@@ -0,0 +1,37 @@
+package kr.codesqaud.chessgame.chess;
+
+import kr.codesqaud.chessgame.exception.InvalidTurnException;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ChessGameTest {
+
+    @Test
+    @DisplayName("백색인 선공인 상테에서 흑백 기물을 이동시키려고 할때 거부된다")
+    public void givenWhitePlayerTurn_whenMoveBlackPiece_thenException() {
+        // given
+        ChessGame game = new ChessGame(new ChessBoard());
+        game.initialize();
+        // when
+        SoftAssertions.assertSoftly(softAssertions -> {
+            softAssertions.assertThatThrownBy(() -> {
+                game.move("a7", "a6");
+            }).isInstanceOf(InvalidTurnException.class);
+        });
+    }
+
+    @Test
+    @DisplayName("백색이 선공인 상태에서 빈칸을 이동시키려고 할때 거부된다.")
+    public void givenWhitePlayerTurn_whenMoveBlank_thenException() {
+        // given
+        ChessGame game = new ChessGame(new ChessBoard());
+        game.initialize();
+        // when
+        SoftAssertions.assertSoftly(softAssertions -> {
+            softAssertions.assertThatThrownBy(() -> {
+                game.move("a3", "a4");
+            }).isInstanceOf(InvalidTurnException.class);
+        });
+    }
+}
