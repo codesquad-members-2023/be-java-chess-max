4c12531a74aee225435e553a42a3a79c839cf383
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-06-13 22:30:00 +0900

    feat: Rank 객체 초기화 기능 구현

--- src/main/java/kr/codesqaud/chessgame/chess/Rank.java
+++ src/main/java/kr/codesqaud/chessgame/chess/Rank.java
@@ -1,10 +1,19 @@
 package kr.codesqaud.chessgame.chess;
 
+import static kr.codesqaud.chessgame.pieces.Position.createPosition;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
+import kr.codesqaud.chessgame.pieces.Bishop;
+import kr.codesqaud.chessgame.pieces.Blank;
+import kr.codesqaud.chessgame.pieces.King;
+import kr.codesqaud.chessgame.pieces.Knight;
+import kr.codesqaud.chessgame.pieces.Pawn;
 import kr.codesqaud.chessgame.pieces.Piece;
+import kr.codesqaud.chessgame.pieces.Queen;
+import kr.codesqaud.chessgame.pieces.Rook;
 import kr.codesqaud.chessgame.pieces.config.Color;
 import kr.codesqaud.chessgame.pieces.config.Type;
 
@@ -15,6 +24,56 @@ public class Rank implements Comparable<Rank> {
 
     public Rank(final int value) {
         this.value = value;
+    }
+
+    public static Rank initializeBlankLine(final int rankValue) {
+        Rank rank = new Rank(rankValue);
+        for (int i = 1; i <= Board.SIZE; i++) {
+            rank.addPiece(Blank.create(createPosition(i, rankValue)));
+        }
+        return rank;
+    }
+
+    public static Rank initializeWhitePieces(final int rankValue) {
+        Rank rank = new Rank(rankValue);
+        rank.addPiece(Rook.createWhite(createPosition(1, rankValue)));
+        rank.addPiece(Knight.createWhite(createPosition(2, rankValue)));
+        rank.addPiece(Bishop.createWhite(createPosition(3, rankValue)));
+        rank.addPiece(Queen.createWhite(createPosition(4, rankValue)));
+        rank.addPiece(King.createWhite(createPosition(5, rankValue)));
+        rank.addPiece(Bishop.createWhite(createPosition(6, rankValue)));
+        rank.addPiece(Knight.createWhite(createPosition(7, rankValue)));
+        rank.addPiece(Rook.createWhite(createPosition(8, rankValue)));
+        return rank;
+    }
+
+    public static Rank initializeWhitePawns(final int rankValue) {
+        Rank rank = new Rank(rankValue);
+        for (int i = 1; i <= Board.SIZE; i++) {
+            rank.addPiece(Pawn.createWhite(createPosition(i, rankValue)));
+        }
+        return rank;
+    }
+
+    public static Rank initializeBlackPawns(final int rankValue) {
+        Rank rank = new Rank(rankValue);
+        for (int i = 1; i <= Board.SIZE; i++) {
+            rank.addPiece(Pawn.createBlack(createPosition(i, rankValue)));
+        }
+        return rank;
+    }
+
+    public static Rank initializeBlackPieces(final int rankValue) {
+        Rank rank = new Rank(rankValue);
+        rank.addPiece(Rook.createBlack(createPosition(1, rankValue)));
+        rank.addPiece(Knight.createBlack(createPosition(2, rankValue)));
+        rank.addPiece(Bishop.createBlack(createPosition(3, rankValue)));
+        rank.addPiece(Queen.createBlack(createPosition(4, rankValue)));
+        rank.addPiece(King.createBlack(createPosition(5, rankValue)));
+        rank.addPiece(Bishop.createBlack(createPosition(6, rankValue)));
+        rank.addPiece(Knight.createBlack(createPosition(7, rankValue)));
+        rank.addPiece(Rook.createBlack(createPosition(8, rankValue)));
+        return rank;
     }
 
     public List<Piece> getPieces() {
@@ -46,6 +105,12 @@ public class Rank implements Comparable<Rank> {
             .count();
     }
 
+    public int countPieces() {
+        return (int) pieces.stream()
+            .filter(piece -> !Objects.equals(piece.getType(), Type.NO_PIECE))
+            .count();
+    }
+
     public boolean isMatchRank(int rank) {
         return this.value == rank;
     }
@@ -54,6 +119,4 @@ public class Rank implements Comparable<Rank> {
     public int compareTo(final Rank rank) {
         return Integer.compare(value, rank.value);
     }
-
-
 }
