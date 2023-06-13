2bee26ce2c6c828642d28915eb3ec4928f46b24e
yeonise <cerealnum@gmail.com>
2023-06-13 18:55:15 +0900

    refactor : [Board.java] 필드명을 board에서 ranks로 변경

--- main/chess/Board.java
+++ main/chess/Board.java
@@ -7,7 +7,6 @@ import chess.piece.Position;
 import chess.piece.Type;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,58 +14,55 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static chess.piece.Type.PAWN;
-import static chess.util.StringUtil.NEW_LINE;
 
 public class Board {
 
-    private final List<Rank> board;
+    private final List<Rank> ranks;
 
     public Board() {
-        this.board = new ArrayList<>(8);
+        this.ranks = new ArrayList<>(8);
     }
 
     public void initialize() {
-        board.add(Rank.initializeWhitePieces(0));
-        board.add(Rank.initializeWhitePawns(1));
-        board.addAll(IntStream.range(0, 4).mapToObj(o -> Rank.createBlankRank(o + 2)).collect(Collectors.toList()));
-        board.add(Rank.initializeBlackPawns(6));
-        board.add(Rank.initializeBlackPieces(7));
+        ranks.add(Rank.initializeWhitePieces(0));
+        ranks.add(Rank.initializeWhitePawns(1));
+        ranks.addAll(IntStream.range(0, 4).mapToObj(o -> Rank.createBlankRank(o + 2)).collect(Collectors.toList()));
+        ranks.add(Rank.initializeBlackPawns(6));
+        ranks.add(Rank.initializeBlackPieces(7));
     }
 
     public void initializeEmpty() {
-        board.addAll(IntStream.range(0, 8).mapToObj(Rank::createBlankRank).collect(Collectors.toList()));
+        ranks.addAll(IntStream.range(0, 8).mapToObj(Rank::createBlankRank).collect(Collectors.toList()));
     }
 
     public int countPieces() {
-        return board.stream().mapToInt(Rank::countPieces).sum();
+        return ranks.stream().mapToInt(Rank::countPieces).sum();
     }
 
     public int countPiecesByColorAndType(Color color, Type type) {
-        return board.stream().mapToInt(rank -> rank.countPiecesByColorAndType(color, type)).sum();
+        return ranks.stream().mapToInt(rank -> rank.countPiecesByColorAndType(color, type)).sum();
     }
 
     public Piece findPiece(String position) {
         final Position findPosition = new Position(position);
-        return board.get(findPosition.getIndexY()).findPiece(findPosition.getIndexX());
+        return ranks.get(findPosition.getIndexY()).findPiece(findPosition.getIndexX());
     }
 
     public List<Piece> findPiecesByColor(Color color) {
-        return board.stream().flatMap(rank -> rank.findPiecesByColor(color).stream()).collect(Collectors.toList());
+        return ranks.stream().flatMap(rank -> rank.findPiecesByColor(color).stream()).collect(Collectors.toList());
     }
 
-    public String show() {
-        final List<Rank> reversedBoard = new ArrayList<>(this.board);
-        Collections.reverse(reversedBoard);
-        return reversedBoard.stream().map(Rank::show).collect(Collectors.joining(NEW_LINE));
+    public List<Rank> getRanks() {
+        return this.ranks;
     }
 
     public void put(final String position, final Piece piece) {
         final Position targetPosition = new Position(position);
-        board.get(targetPosition.getIndexY()).put(targetPosition.getIndexX(), piece);
+        ranks.get(targetPosition.getIndexY()).put(targetPosition.getIndexX(), piece);
     }
 
     public void put(final Position targetPosition, final Piece piece) {
-        board.get(targetPosition.getIndexY()).put(targetPosition.getIndexX(), piece);
+        ranks.get(targetPosition.getIndexY()).put(targetPosition.getIndexX(), piece);
     }
 
     public void move(final String sourcePosition, final String targetPosition) {
