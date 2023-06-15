92ff18f61116f333a45097fddc781a35b81d07f3
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-06-15 14:16:30 +0900

    feat: 명령어 소개, 시작 명령어 소개, 현재 차례를 보여주는 기능 구현

--- src/main/java/kr/codesqaud/chessgame/view/ConsoleView.java
+++ src/main/java/kr/codesqaud/chessgame/view/ConsoleView.java
@@ -1,5 +1,7 @@
 package kr.codesqaud.chessgame.view;
 
+import kr.codesqaud.chessgame.chess.pieces.config.Color;
+
 public class ConsoleView implements View {
 
     @Override
@@ -10,5 +12,26 @@ public class ConsoleView implements View {
     @Override
     public void showPrompt() {
         System.out.print("> ");
+    }
+
+    @Override
+    public void showCommands() {
+        println("기물이동 : move {시작} {도착} ex) move a2 a3");
+        println("게임종료 : end");
+    }
+
+    @Override
+    public void showStartCommands() {
+        println("체스 게임");
+        println("게임 시작: start, 게임 종료: end");
+    }
+
+    @Override
+    public void showCurrentTurn(final Color color) {
+        println("현재 차례 : " + color);
+    }
+
+    private void println(String text) {
+        System.out.println(text);
     }
 }
--- src/main/java/kr/codesqaud/chessgame/view/View.java
+++ src/main/java/kr/codesqaud/chessgame/view/View.java
@@ -1,8 +1,16 @@
 package kr.codesqaud.chessgame.view;
 
+import kr.codesqaud.chessgame.chess.pieces.config.Color;
+
 public interface View {
-    
+
     void showMessage(String message);
 
     void showPrompt();
+
+    void showCommands();
+
+    void showStartCommands();
+
+    void showCurrentTurn(Color currentTurn);
 }
