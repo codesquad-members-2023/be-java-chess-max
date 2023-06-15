8fc6d2a0bc9bca3381e5d4de4a65270e623c541a
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-06-15 14:18:58 +0900

    fix: 체스 게임 컨트롤러 코드 수정

--- src/main/java/kr/codesqaud/chessgame/controller/ConsoleChessGameController.java
+++ src/main/java/kr/codesqaud/chessgame/controller/ConsoleChessGameController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Objects;
+import java.util.regex.Pattern;
 import kr.codesqaud.chessgame.chess.ChessGame;
 import kr.codesqaud.chessgame.chess.board.ChessBoard;
 import kr.codesqaud.chessgame.exception.InvalidMovingPieceException;
@@ -14,6 +15,9 @@ import kr.codesqaud.chessgame.view.View;
 
 public class ConsoleChessGameController implements ChessGameController {
 
+    private static final Pattern moveCommandPattern = Pattern.compile("move [a-h][1-8] [a-h][1-8]");
+    private static final String START_COMMAND = "start";
+    private static final String END_COMMAND = "end";
     private final ChessGame game;
     private final View view;
 
@@ -23,72 +27,56 @@ public class ConsoleChessGameController implements ChessGameController {
     }
 
     public void service(final InputStream in) {
-        view.showMessage("체스 게임");
-        view.showMessage("게임 시작: start, 게임 종료: end");
-
         try (BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
-            while (true) {
-                view.showPrompt();
-                String command = br.readLine();
-                if (checkStartCommand(command)) {
-                    view.showMessage("게임을 시작합니다.");
-                    game.initialize();
-                    view.showMessage(game.showBoard());
-                    break;
-                } else if (checkEndCommand(command)) {
-                    view.showMessage("게임을 종료합니다.");
-                    return;
-                }
-                view.showMessage("명령어가 올바르지 않습니다. : " + command);
-            }
+            view.showStartCommands();
+            view.showPrompt();
+            String command = br.readLine();
+            boolean isRunning = executeStartEndCommand(command);
 
-            while (true) {
-                view.showMessage("기물이동 : move {시작} {도착} ex) move a2 a3");
-                view.showMessage("게임종료 : end");
-                view.showMessage("현재 차례 : " + game.getCurrentTurn());
-                view.showPrompt();
-                String command = br.readLine();
-
-                // 게임 종료
-                if (checkEndCommand(command)) {
-                    break;
-                }
-
-                // 명령어 처리
-                if (processCommand(command)) {
-                    game.nextTurn();
-                }
+            while (isRunning) {
+                showIntroduce();
+                command = br.readLine();
+                isRunning = executeCommand(command);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private boolean checkEndCommand(final String command) {
-        return Objects.equals(command, "end");
-    }
-
-    private boolean checkStartCommand(final String command) {
-        return Objects.equals(command, "start");
-    }
-
-    private boolean processCommand(final String command) {
-        if (command.startsWith("move")) {
-            try {
-                return processMoveCommand(command);
-            } catch (ArrayIndexOutOfBoundsException e) {
-                view.showMessage("올바르지 않은 move 명령어 형식입니다.");
-            }
+    private boolean executeStartEndCommand(final String command) {
+        if (Objects.equals(command, START_COMMAND)) {
+            view.showMessage("게임을 시작합니다.");
+            game.initialize();
+            view.showMessage(game.showBoard());
+            return true;
+        } else if (Objects.equals(command, END_COMMAND)) {
+            view.showMessage("게임을 종료합니다.");
+            return false;
         } else {
-            view.showMessage(command + " 명령어는 지원하지 않습니다.");
+            view.showMessage("명령어가 올바르지 않습니다. 다시 시작해주세요 : " + command);
+            return false;
         }
-        return false;
     }
 
-    private boolean processMoveCommand(final String command) {
+    private boolean executeCommand(final String command) {
+        if (Objects.equals(command, END_COMMAND)) {
+            return false;
+        } else if (moveCommandPattern.matcher(command).matches()) {
+            boolean result = executeMoveCommand(command);
+            if (result) {
+                game.nextTurn();
+            }
+            return true;
+        } else {
+            view.showMessage("명령어가 올바르지 않습니다. : " + command);
+            return true;
+        }
+    }
+
+    private boolean executeMoveCommand(final String command) {
         final int SOURCE_POSITION_INDEX = 1;
         final int TARGET_POSITION_INDEX = 2;
-        final String BLANK = " ";
+        final String BLANK = "\\s";
         String[] commands = command.split(BLANK);
 
         try {
@@ -99,5 +87,11 @@ public class ConsoleChessGameController implements ChessGameController {
             view.showMessage(e.getMessage());
         }
         return false;
+    }
+
+    private void showIntroduce() {
+        view.showCommands();
+        view.showCurrentTurn(game.getCurrentTurn());
+        view.showPrompt();
     }
 }
